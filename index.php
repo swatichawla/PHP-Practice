a34a6ac57bc87696cdfe7b84fa90d5c6dea3f9b6
Swati Chawla <sc2wt@virginia.edu>
2015-01-30 15:16:57 -0500

    first commit

---
+++ index.php
@@ -0,0 +1,10 @@
+<html>
+<head>
+<title> PHP Homework, Exercise 1, Swati Yay!!! </title>
+
+<body>
+<h1> My First PHP File </h1>
+<p> <?php echo 'Let us learn PHP! Never too old to learn!'; ?> </p>
+</body>
+</head>
+</html>
