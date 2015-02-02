9eb930840568c1d1831b7166cbf3d71a524efa8a
Swati Chawla <sc2wt@virginia.edu>
2015-02-02 14:01:56 -0500

    changes to homework

--- index.php
+++ index.php
@@ -1,10 +1,14 @@
 <html>
 <head>
-<title> PHP Homework, Exercise 1, Swati Yay!!! </title>
+<title> PHP Homework, Exercise 1, Swati!!! </title>
 
 <body>
 <h1> My First PHP File </h1>
 <p> <?php echo 'Let us learn PHP! Never too old to learn!'; ?> </p>
+<p> <?php echo 'My Address'; ?> </p>
+<p> <?php echo 'US:'; ?> </p>
+<p> <?php echo '520 Seymour Road, Apartment B7, Charlottesville, VA 22903'; ?> </p>
+<p> <?php echo 'India'; ?> </p>
 </body>
 </head>
 </html>
