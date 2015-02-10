fced5ac537a912ec7b9b4632923cef57ed8b715c
Swati Chawla <sc2wt@virginia.edu>
2015-02-10 11:06:01 -0500

    homework for this week

---
+++ homework.php
@@ -0,0 +1,25 @@
+<html>
+<head>
+<title> Time Travel
+</title>
+</head>
+<body>
+<?php
+$tests = [1823, 1995, 3043];
+/**
+* Test if a year is from the past, present, or future
+*/
+/
+ 
+function test_year($year)
+{
+switch ($year) 
+{
+	case ($year < 1900);
+
+}
+if ( $year < 1900 )
+}
+?>
+</body>
+</html>
--- index.php
+++ index.php
@@ -1,10 +1,109 @@
 <html>
 <head>
-<title> PHP Homework, Exercise 1, Swati!!! </title>
+<title> PHP Homework, Exercise 2, Swati!!! </title>
 
 <body>
 <h1> My First PHP File </h1>
-<p> <?php echo 'Let us learn PHP! Never too old to learn!'; ?> </p>
+<p> <?php echo 'Let us learn PHP! Never too old to learn! Haha'; ?> </p>
+<?php
+function multiply ($x,$y)
+{
+	return $x * $y;
+}
+
+$values = array ( 
+	4 => 2,
+	0 => 4,
+	900 => 32,
+	29999 => 23
+	);
+foreach ($values as $x => $y) {
+	echo "<li>" . multiply ($x,$y) . "</li>";
+}
+
+?>
+
+<?php
+function convert ($second)
+{
+	return $second/(31557600);
+} 
+$second = array(600000000,60,40000.33);
+foreach ($second as $seconds) 
+{
+	$year = convert ($seconds);
+	echo "<li>" . $seconds . " seconds is " . $year . "." . "</li>";
+
+}
+
+?>
+
+
+<?php
+function divide ($x,$y)
+{
+	return $x / $y;
+}
+
+$values = array ( 
+	4 => 2,
+	0 => 4,
+	4 => 0,
+	900 => 32
+	);
+foreach ($values as $x => $y) {
+	if ($y == 0) {
+		echo "<li>" . "You cannot divide by zero, duh!" . "</li>";
+	}
+	else {
+	echo "<li>" . divide ($x,$y) . "</li>";
+	}
+}
+
+
+?>
+
+<?php
+function compare ($x,$y)
+{
+	
+	if ($x < $y) {
+		return $x ;
+	}
+	elseif ($x == $y) {
+	return  "The numbers are equal." ;
+	}
+	else {
+	return	$y ;
+	}
+}
+
+
+?>
+
+<?php
+function generate ($year)
+{
+	
+	if ($year < 1900) {
+
+		echo "<li>" . "I have a mattress for thee, traveller from distant lands!" . "</li>";
+
+	}
+	elseif ($year < 2020) {
+	echo "<li>" . "I have a mattress for you, dear friend!" . "</li>";
+	}
+	else {
+	echo "<li>" . "I have a mattress for ya, dude!" . "</li>";
+	}
+
+
+}
+generate (1823);
+generate (1995);
+generate (3043);
+
+?>
 <?php
 $address= array (
 	"name"=>"Swati Chawla",
@@ -23,11 +122,15 @@ $address= array (
 
 <h3> Authors and Death Dates </h3>
 <?php
-$author= array ("Charles Dickens", "William Thackeray", "Anthony Trollope", "GM Hopkins");
-$deathdate= array ("1870", "1863","1882", "1889");
+$authordeathyear= array (
+	"Charles Dickens" => "1870", 
+	"William Thackeray" => "1863", 
+	"Anthony Trollope" => "!882", 
+	"GM Hopkins" => "1889");
 
-foreach ($author as $key=>$name) {
-	echo $name. " died in " .$deathdate[$key].". <br/>";
+
+foreach ($authordeathyear as $author=>$deathyear) {
+	echo $author. " died in " .$deathyear.". <br/>";
 }
 ?>
 </body>
